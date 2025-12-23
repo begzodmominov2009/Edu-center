219b2dddf7a8aa67628bfd4370b3a742655d68d9
begzodmominov2009 <begzodmominov32@gmail.com>
2025-12-23 03:59:03 +0300

    add login changes

--- src/modules/Login/LoginPage.jsx
+++ src/modules/Login/LoginPage.jsx
@@ -4,15 +4,6 @@ const LoginPage = () => {
   return (
     <div>
       <a href="/main">admin panelga kirish</a>
-      <div className="flex items-center justify-center w-full min-h-screen bg-gray-100">
-        <div className="w-full max-w-sm mx-auto bg-white p-3 text-center font-medium text-blue-600 shadow-lg rounded-lg">
-          <h1 className="mb-3 text-[18px]">Online Nurse Registration</h1>
-
-    <form>
-      <input type="text" />
-    </form>
-        </div>
-      </div>
     </div>
   )
 }
